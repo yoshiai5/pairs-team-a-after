41014ff325a3dedbc8eeab3749a75f510cd422ea
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-06 15:48:36 +0900

    メッセージ機能、インデックスページの追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,24 @@
 class GroupsController < ApplicationController
   def index
+    @groups =[]
+
+    current_user.matchers.each do |match_user|
+      if MatchUser.find_by(before_user_id: current_user.id, after_user_id: match_user.id)
+        @groups_a = MatchUser.find_by(before_user_id: current_user.id, after_user_id: match_user.id)
+    @groups << @groups_a
+
+      elsif MatchUser.find_by(after_user_id: current_user.id, before_user_id: match_user.id)
+        @groups_a = MatchUser.find_by(after_user_id: current_user.id, before_user_id: match_user.id)
+    @groups << @groups_a
+
+      else
+        @group = Group.new
+        @group.save
+        @groups_a = MatchUser.create(before_user_id: current_user.id, after_user_id: match_user.id, group_id:@group.id)
+    @groups << @groups_a
+
+      end
+    end
+
   end
 end
