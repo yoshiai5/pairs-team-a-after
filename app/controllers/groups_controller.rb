bce65369ee678a41a7dd352015f6138834c350d4
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-09 17:28:30 +0900

    グループindexページの修正
    ・leftコンテンツ、マッチング者表示ロジックの修正
    ・マッチング者表示画像の条件分岐
    （有り、無し（男性or女性）

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,19 +4,16 @@ class GroupsController < ApplicationController
 
     current_user.matchers.each do |match_user|
       if MatchUser.find_by(before_user_id: current_user.id, after_user_id: match_user.id)
-        @groups_a = MatchUser.find_by(before_user_id: current_user.id, after_user_id: match_user.id)
-    @groups << @groups_a
-
+        @groups_matchuser = MatchUser.find_by(before_user_id: current_user.id, after_user_id: match_user.id)
+        @groups << @groups_matchuser
       elsif MatchUser.find_by(after_user_id: current_user.id, before_user_id: match_user.id)
-        @groups_a = MatchUser.find_by(after_user_id: current_user.id, before_user_id: match_user.id)
-    @groups << @groups_a
-
+        @groups_matchuser = MatchUser.find_by(after_user_id: current_user.id, before_user_id: match_user.id)
+        @groups << @groups_matchuser
       else
         @group = Group.new
         @group.save
-        @groups_a = MatchUser.create(before_user_id: current_user.id, after_user_id: match_user.id, group_id:@group.id)
-    @groups << @groups_a
-
+        @groups_matchuser = MatchUser.create(before_user_id: current_user.id, after_user_id: match_user.id, group_id:@group.id)
+        @groups << @groups_matchuser
       end
     end
 
