e609eac2bcabc0acff0bcd1d4cf68375d87c8981
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-11 16:03:59 +0900

    Revert "修正3回目"
    This reverts commit 2f2349167ff242fb36f7a943b31c4d15d3c9244a.

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
-# S3を使用しているかを判定するためのメソッド。（ローカル環境ではS3を使わないため）
-  if Rails.env.production?
-  # public配下にキャッシュができると参照されてしまうので、予め変えておく。
-    config.cache_dir = "#{Rails.root}/tmp/uploads"
-  end
