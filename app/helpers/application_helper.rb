f7c86804abb4921d0b0a6756d8d6f6cc22f62ee0
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-25 18:41:04 +0900

    日時のヘルパーメソッドの追加、修正。選択時のscssの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,16 @@
 module ApplicationHelper
 
   def format_posted_timeHM(date)
-  date.strftime("%H:%M:%S")
+    date.strftime("%H:%M:%S")
   end
 
   def format_posted_timeYMD(date)
-  date.strftime("%m月%d日")
+    date.strftime("%m月%d日")
+  end
+
+  def format_posted_time(time)
+    time.in_time_zone("Tokyo") #タイムゾーン変換
+    @time = time.in_time_zone("Tokyo").strftime("%Y-%m-%d %H:%M:%S") #不要な表示削除
   end
 
 end
