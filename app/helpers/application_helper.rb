225f300502b3dc7305e2e6064de3f9160659fb69
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-25 20:14:19 +0900

    メッセージ機能、送信日時の表示（動的）
    ・ヘルパーメソッドの設定
    ・ビューへの反映

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,11 @@ module ApplicationHelper
     date.strftime("%m月%d日")
   end
 
+  def format_posted_timeMDW(date)
+    date.strftime("%m/%d(#{%w(日 月 火 水 木 金 土)[date.wday]})")
+    # 『%w』で曜日が数字で出てくるので（0~6）、対応表を定義して取り出す。
+  end
+
   def format_posted_time(time)
     time.in_time_zone("Tokyo") #タイムゾーン変換
     @time = time.in_time_zone("Tokyo").strftime("%Y-%m-%d %H:%M:%S") #不要な表示削除
